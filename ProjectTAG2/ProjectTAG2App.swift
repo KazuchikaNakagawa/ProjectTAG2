4c4fc17a3787a65fb00629f58d2712149e480568
KazuchikaNakagawa <KazuchikaNakagawa@gmail.com>
2024-10-14 21:56:09 +0900

    Initial Commit

---
+++ ProjectTAG2/ProjectTAG2App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTAG2App.swift
+//  ProjectTAG2
+//
+//  Created by かずちか on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTAG2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectTAG2Tests/ProjectTAG2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTAG2Tests.swift
+//  ProjectTAG2Tests
+//
+//  Created by かずちか on 2024/10/14.
+//
+
+import Testing
+@testable import ProjectTAG2
+
+struct ProjectTAG2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
